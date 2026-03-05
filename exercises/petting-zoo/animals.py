11810f62e45f7981ca152ac58d199b8c2224d332
Jack Gardner <jtgardner99@gmail.com>
2026-03-05 16:07:37 -0600

    chapter 2 of column 2 done

---
+++ exercises/petting-zoo/animals.py
@@ -0,0 +1,143 @@
+from datetime import date
+
+class Llama:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class Goat:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class Sheep:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class MiniatureDonkey:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class Alpaca:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class Rabbit:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+class KingSnake:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class RatSnake:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class CoralSnake:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class Python:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class Mallard:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class KoiFish:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class GoldFish:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class Goose:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class MiniaturePony:
+
+    def __init__(self):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+miss_fuzz = Llama()
+liam = Goat()
+olivia = Sheep()
+noah = MiniatureDonkey()
+amelia = Alpaca()
+oliver = Rabbit()
+sophia = KingSnake()
+elijah = RatSnake()
+emma = CoralSnake()
+mateo = Python()
+charlotte = Mallard()
+levi = KoiFish()
+isabella = GoldFish()
+lucas = Goose()
+mia = MiniaturePony()
+
+miss_fuzz.name = "Miss Fuzz"
+miss_fuzz.species = "domestic llama"
+
+
