5b20316bdad09a847f023748739305bfbbf475a6
Jack Gardner <jtgardner99@gmail.com>
2026-03-06 10:29:32 -0600

    shift property added to animal classes

--- exercises/petting-zoo/index.py
+++ exercises/petting-zoo/index.py
@@ -4,12 +4,12 @@ from slithering import CoralSnake, KingSnake, RatSnake, Python
 from swimming import GoldFish, KoiFish, Mallard, Goose
 
 
-miss_fuzz = Llama("Miss Fuzz", "Llama")
-liam = Goat("Liam", "Goat")
-olivia = Sheep("Olivia", "Sheep")
-noah = MiniatureDonkey("Noah", "Miniature Donkey")
-amelia = Alpaca("Amelia", "Alpaca")
-oliver = Rabbit("Oliver", "Rabbit")
+miss_fuzz = Llama("Miss Fuzz", "Llama", "midday")
+liam = Goat("Liam", "Goat", "morning")
+olivia = Sheep("Olivia", "Sheep", "morning")
+noah = MiniatureDonkey("Noah", "Miniature Donkey", "midday")
+amelia = Alpaca("Amelia", "Alpaca", "midday")
+oliver = Rabbit("Oliver", "Rabbit", "midday")
 sophia = KingSnake("Sophia", "King Snake")
 elijah = RatSnake("Elijah", "Rat Snake")
 emma = CoralSnake("Emma", "Coral Snake")
@@ -18,8 +18,8 @@ charlotte = Mallard("Charlotte", "Mallard")
 levi = KoiFish("Levi", "Koi Fish")
 isabella = GoldFish("Isabella", "Gold Fish")
 lucas = Goose("Lucas", "Goose")
-mia = MiniaturePony("Mia", "Miniature Pony")
+mia = MiniaturePony("Mia", "Miniature Pony", "morning")
 
-print(miss_fuzz)
+print(f"{liam.name} the {liam.species} is available to pet during the {liam.shift} shift.")
 
 
